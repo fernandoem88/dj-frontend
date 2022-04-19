7830e43eaa232ee2885fbffffcedb01524ff400d
fernando ekutsu mondele <f.ekutsumondele@testbirds.com>
2022-04-19 17:41:31 +0200

    api url

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "/";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://dj-events-prova-1.herokuapp.com";
 export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
 export const GEOCODE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || "";
